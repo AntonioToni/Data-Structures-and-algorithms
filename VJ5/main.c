738c9a042ec6b5ac9bf4f2705a28c1ea7630f664
Antonio Toni <antonio.gaming10@gmail.com>
2022-01-13 11:52:42 +0100

    Added fixup and fixdown functions

--- VJ5/main.c
+++ VJ5/main.c
@@ -1,11 +1,48 @@
 #include <stdio.h>
 #include <conio.h>
 
-struct PriorityQueue
+typedef struct
+{
+    void *x;
+    int priority;
+}PriorityQueue;
+
+void fixup(PriorityQueue *array, int c)
 {
     int x;
-    int priority;
-}pq[10000];
+    int r = (c-1) / 2;
+    if (array[r].priority < array[c].priority)
+    {
+        x = array[r].priority;
+        array[r].priority = array[c].priority;
+        array[c].priority = x;
+        fixup(array, r);
+    }
+}
+
+void fixdown(PriorityQueue *array, int r)
+{
+    int x;
+    int l = 2 * r + 1;
+    int d = 2 * r + 2;
+    if (array[r].priority < array[l].priority && array[r].priority < array[d].priority)
+    {
+        if (array[l].priority > array[d].priority)
+        {
+            x = array[r].priority;
+            array[r].priority = array[l].priority;
+            array[l].priority = x;
+            fixdown(array, l);
+        }
+        else
+        {
+            x = array[r].priority;
+            array[r].priority = array[d].priority;
+            array[d].priority = x;
+            fixdown(array, d);
+        }
+    }
+}
 
 int add()
 {
