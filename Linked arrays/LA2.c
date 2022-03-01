1f34c274a4d7bb75df6aaca9eeee6242db1a9672
Antonio <antonio.gaming10@gmail.com>
2022-03-01 14:49:06 +0100

    added funk

--- Linked arrays/LA2.c
+++ Linked arrays/LA2.c
@@ -20,6 +20,7 @@ void print(Array* arr) //print array
             printf("%d, ", arr->num);
         }
         arr = arr->next;
+        //sleep(1);
     }
 }
 
@@ -36,6 +37,10 @@ int checkarr(Array* arr1, Array* arr2)
 {
     Array* tmp1 = arr1;
     Array* tmp2 = arr2;
+    if (arr1 == NULL)
+    {
+        return;
+    }
     while (tmp1 != NULL)
     {
         while (tmp2 != NULL)
@@ -49,7 +54,6 @@ int checkarr(Array* arr1, Array* arr2)
             {
                 return 0;
             }
-            
             else
             {
                 tmp2=tmp2->next;
@@ -92,6 +96,62 @@ Array* delFirstLast(Array* arr1)
     return arr1;
 }
 
+Array* delDivisable(Array *arr, int x)
+{
+    if (arr == NULL)
+    {
+        return;
+    }
+    Array *new;
+    if (arr->num%x==0)
+    {
+        Array* del = arr;
+        arr = arr->next;
+        free(del);
+    }
+    
+    Array *tmp = arr;
+
+    while (tmp->next != NULL)
+    {
+        if (tmp->next->num % x == 0)
+        {
+            printf("%d\n", tmp->next->num);
+            Array *del = tmp->next;
+            tmp->next = tmp->next->next;
+            free(del);
+        }
+        else{
+            new->next=tmp;
+            tmp = tmp->next;
+        }
+    }
+    arr= new;
+    return arr;
+}
+
+Array* funk(Array* arr, int x, int y)
+{
+    int i=0;
+    Array* tmp = arr;
+    while (tmp->next!=NULL)
+    {
+        if (i >= x && i < y-1)
+        {
+            Array* del = tmp->next;
+            tmp->next=tmp->next->next;
+            tmp= tmp->next;
+            free(del);
+        }
+        else
+        {
+            tmp=tmp->next;
+        }
+        i++;
+    }
+    return arr;
+}
+
 int main()
 {
     //construct arr1 4 3 2
@@ -100,16 +160,22 @@ int main()
     arr1->next = NULL;
     arr1 = insertFirst(arr1, 3);
     arr1 = insertFirst(arr1, 4);
-    print(arr1);
+    //print(arr1);
     //construct arr2 2 1 4
     Array* arr2 = (Array*)malloc(sizeof(Array));
-    arr2->num=4;
+    arr2->num=3;
     arr2->next = NULL;
+    arr2 = insertFirst(arr2, 4);
     arr2 = insertFirst(arr2, 3);
-    arr2 = insertFirst(arr2, 2);
     arr2 = insertFirst(arr2, 6);
+    arr2 = insertFirst(arr2, 5);
     print(arr2);
-    printf("%d", checkarr(arr1,arr2));
-    arr1 = delFirstLast(arr1);
-    print(arr1);
+    //printf("%d", checkarr(arr1,arr2));
+    //arr1 = delFirstLast(arr1);
+    //print(arr1);
+    //arr2 = delDivisable(arr2, 2);
+    //print(arr2);
+    arr2 = funk(arr2, 1,3);
+    print(arr2);
+    system("pause");
 }
